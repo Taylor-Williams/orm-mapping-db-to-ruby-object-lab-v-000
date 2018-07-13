8d80dc01f272752b3a5f94f1cf4fe3e05d91f97e
Taylor Williams <taylor@thewilliams.net>
2018-07-13 23:51:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,6 +19,7 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    DB[:conn].execute("SELECT * FROM students WHERE students.name = ?", name)
   end
 
   def save
