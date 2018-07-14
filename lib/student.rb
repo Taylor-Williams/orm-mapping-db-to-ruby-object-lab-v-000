57304fb58123ac03c89db4cfdae757aae3ff6883
Taylor Williams <taylor@thewilliams.net>
2018-07-14 00:00:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,9 +35,9 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    self.first_student_in_grade_10(1)
+    self.first_X_students_in_grade_10(1)
   end
-  
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
